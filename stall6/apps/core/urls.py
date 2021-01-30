4b256a7d1a92df0fb5539139c214821c84c5ec9d
Richard Rosario <catrosits@gmail.com>
2021-01-30 10:55:08 +0800

    setting up the static files directory and home views,urls

--- stall6/apps/core/urls.py
+++ stall6/apps/core/urls.py
@@ -3,5 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', frontpage, name='frontpage' ),
-    path('', contact, name='contact' ),
+    path('contact/', contact, name='contact' ),
 ]
--- stall6/stall6/settings.py
+++ stall6/stall6/settings.py
@@ -121,6 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES = [
-    BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR,  'static'
 ]
