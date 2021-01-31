e21e5e6e00728dc9d14299c3aae321bb028665b5
Richard Rosario <catrosits@gmail.com>
2021-01-31 15:10:14 +0800

    creating the initial models and views

--- stall6/apps/vendor/views.py
+++ stall6/apps/vendor/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Vendor
+
+def vendorslist(request):
+    vendors = get_object_or_404(Vendor, )
+
+    context = {
+        'vendors': vendors
+    }
+
+    return render(request, 'frontpage.html', context)
